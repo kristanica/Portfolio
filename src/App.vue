5f77491ece06e2216551ea2c72e256dfaaf28c88
Leewell <LeewellC21@gmail.com>
2026-03-29 12:45:46 +0800

    Initial Commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>You did it!</h1>
+  <div class="bg-blue-500 text-white p-4">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </div>
+</template>
+
+<style scoped></style>
